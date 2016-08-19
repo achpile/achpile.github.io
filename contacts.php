125f4126a3388e977ac05f1e0712ab991f0687c4
Fedosov Alexander <achpile@gmail.com>
2016-08-19 17:45:57 +0300

    Added donations

--- contacts.php
+++ contacts.php
@@ -11,13 +11,14 @@
 
 	set_section_header('Contacts');
 	set_links_start();
-	set_link('vimeo'    , 'VK'      , 'https://vk.com/achpile');
-	set_link('facebook2', 'Facebook', 'https://facebook.com/alexander.fedosov.75');
-	set_link('youtube2' , 'Youtube' , 'http://youtube.com/user/Achpile');
-	set_link('twitter2' , 'Twitter' , 'https://twitter.com/achpile');
-	set_link('github3'  , 'GitHub'  , 'https://github.com/achpile');
-	set_link('sfml'     , 'SFML'    , 'http://en.sfml-dev.org/forums/index.php?action=profile;u=10826');
-	set_link('mail4'    , 'E-Mail'  , 'mailto:achpile@gmail.com');
+	set_link('vimeo'    , 'VK'       , 'https://vk.com/achpile');
+	set_link('facebook2', 'Facebook' , 'https://facebook.com/alexander.fedosov.75');
+	set_link('youtube2' , 'Youtube'  , 'http://youtube.com/user/Achpile');
+	set_link('twitter2' , 'Twitter'  , 'https://twitter.com/achpile');
+	set_link('github3'  , 'GitHub'   , 'https://github.com/achpile');
+	set_link('sfml'     , 'SFML'     , 'http://en.sfml-dev.org/forums/index.php?action=profile;u=10826');
+	set_link('mail4'    , 'E-Mail'   , 'mailto:achpile@gmail.com');
+	set_link('coin'     , 'Donations', 'https://money.yandex.ru/to/410011328479191');
 	set_links_end();
 
 //######################################################################
