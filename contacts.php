abce2e2a566e4efee02fce40f58aa0a5ccb866c5
Fedosov Alexander <achpile@gmail.com>
2017-11-26 15:27:45 +0300

    Added LinkedIn profile

--- contacts.php
+++ contacts.php
@@ -15,6 +15,7 @@
 	set_link('facebook2', 'Facebook' , 'https://facebook.com/alexander.fedosov.75');
 	set_link('youtube2' , 'Youtube'  , 'http://youtube.com/user/Achpile');
 	set_link('twitter2' , 'Twitter'  , 'https://twitter.com/achpile');
+	set_link('linkedin' , 'LinkedIn' , 'https://www.linkedin.com/in/achpile/');
 	set_link('github3'  , 'GitHub'   , 'https://github.com/achpile');
 	set_link('sfml'     , 'SFML'     , 'http://en.sfml-dev.org/forums/index.php?action=profile;u=10826');
 	set_link('mail4'    , 'E-Mail'   , 'mailto:achpile@gmail.com');
